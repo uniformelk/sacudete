bfa53bbe8d21d0f9ca7216521a46eea339e3b693
Cristian Orjuela <tecnologias@devsgo.com.co>
2021-11-08 12:26:54 -0500

    correcciones home pendientes y previa mundo sacudete

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,87 @@ $(window).on("load resize", function() {
   }
 });
 
+//Swipper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 7,
+  spaceBetween: 30,
+  slidesPerGroup: 7,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+var swiper = new Swiper(".mySwiper-mobile", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 2,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+$('img[usemap]').rwdImageMaps();
+	
+	
+$("#mapa1").click(function(){
+  $('#exampleModalLabel').text("Region 1");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+$("#mapa2").click(function(){
+  $('#exampleModalLabel').text("Region 2");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+$("#mapa3").click(function(){
+  $('#exampleModalLabel').text("Region 3");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+$("#mapa4").click(function(){
+  $('#exampleModalLabel').text("Region 4");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+$("#mapa5").click(function(){
+  $('#exampleModalLabel').text("Region 5");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+$("#mapa6").click(function(){
+  $('#exampleModalLabel').text("Region 6");
+  $("#modalText").text("XXX Adolescentes y jóvenes beneficiados");
+  $("#exampleModal").modal("show");
+})
+
+var swiper = new Swiper(".mySwiper-sacudetetv1", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: "",
+    prevEl: "",
+  },
+});
 });
